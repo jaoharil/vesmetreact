39d891b19ff82d0c912bc40713cff40ffec6428a
jaoharil <jaoharilmaknun2904@gmail.com>
2024-12-03 09:29:37 +0800

    rename footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { UpdateFollower } from 'react-mouse-follower';
 import Products from './components/Products/Products';
 import Banner from './components/Banner/Banner';
 import Footer from './components/footer/Footer';
+
 const App = () => {
   return (
     <main className="overflow-x-hidden">
